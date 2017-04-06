f526db288ce29e2cf01c952e4c79cf32622eacb1
Jarred Binder <jarredbinder@gmail.com>
2017-04-06 12:26:36 -0500

    setup
    First commit, basically just created the project

---
+++ src/edu/ilstu/alarms/Alarm.java
@@ -0,0 +1,14 @@
+/**
+ * Project: IT226GroupProject2
+ * @author Jarred/Jerry Binder
+ * Created Apr 6, 2017 12:21:02 PM
+ */
+package edu.ilstu.alarms;
+
+/**
+ * @author jarre
+ *
+ */
+public class Alarm {
+
+}
---
+++ src/edu/ilstu/it/swing/ui/AlarmClockFrame.java
@@ -0,0 +1,46 @@
+/**
+ * Project: IT226GroupProject2
+ * @author Jarred Binder
+ * Created Apr 6, 2017 12:19:38 PM
+ */
+package edu.ilstu.it.swing.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AlarmClockFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AlarmClockFrame frame = new AlarmClockFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AlarmClockFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
