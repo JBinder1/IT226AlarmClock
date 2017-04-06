7c52c54bf792c04cc67c1a4c3356f9b8045870c3
Ryan Greene <rabrg96@gmail.com>
2017-04-06 13:58:11 -0500

    Added IO & alert test

--- src/edu/ilstu/alarms/AlarmIO.java
+++ src/edu/ilstu/alarms/AlarmIO.java
@@ -3,12 +3,28 @@ package edu.ilstu.alarms;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class AlarmIO {
 
 	private static final String PATH = "alarms.csv";
+
+	public static void main(final String[] args) throws InterruptedException {
+		List<Alarm> alarms = new ArrayList<>();
+		alarms.add(AlarmFactory.createAlarm(1, "Alarm one!"));
+		alarms.add(AlarmFactory.createAlarm(2));
+		
+		saveAlarms(alarms);
+		
+		alarms = loadAlarms();
+		for (final Alarm alarm : alarms)
+			alarm.startTimer();
+
+		for (;;)
+			Thread.sleep(100);
+	}
 
 	public static List<Alarm> loadAlarms() {
 		final List<Alarm> alarms = new ArrayList<>();
@@ -17,6 +33,7 @@ public final class AlarmIO {
 			for (final String line : lines) {
 				final String[] split = line.split(",");
 				final long time = Long.parseLong(split[0]);
+				// FIXME: broken if message contains comma
 				alarms.add(split.length == 1 ? AlarmFactory.createAlarm(time) : AlarmFactory.createAlarm(time, split[1]));
 			}
 		} catch (final IOException e) {
@@ -34,6 +51,11 @@ public final class AlarmIO {
 			if (alarm.getMessage().isPresent())
 				builder.append(',').append(alarm.getMessage().get());
 			lines.add(builder.toString());
+		}
+		try {
+			Files.write(Paths.get(PATH), lines, StandardOpenOption.CREATE);
+		} catch (final IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
