c3c724e758207511050087d709a4bff957026006
Ryan Greene <rabrg96@gmail.com>
2017-04-06 13:16:44 -0500

    Removed rawDate from Alarm

--- src/edu/ilstu/alarms/Alarm.java
+++ src/edu/ilstu/alarms/Alarm.java
@@ -1,31 +1,20 @@
 package edu.ilstu.alarms;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
 
 public class Alarm {
 
-	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MM d yyyy HH:mm a");
-
-	private final String rawDate;
 	private final Date date;
 	private final Optional<String> message;
 
-	public Alarm(final String rawDate) throws ParseException {
-		this(rawDate, null);
+	public Alarm(final Date date) {
+		this(date, null);
 	}
 
-	public Alarm(final String rawDate, final String message) throws ParseException {
-		this.rawDate = rawDate;
-		this.date = DATE_FORMAT.parse(rawDate);
+	public Alarm(final Date date, final String message) {
+		this.date = date;
 		this.message = Optional.ofNullable(message);
-	}
-
-	public String getRawDate() {
-		return rawDate;
 	}
 
 	public Date getDate() {
