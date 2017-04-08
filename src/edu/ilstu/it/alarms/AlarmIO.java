55c3d89a5857bf1d92f4e931f1a0e12f23677548
Ryan Greene <rlgree1@ilstu.edu>
2017-04-08 11:50:36 -0500

    Switched from CSV to XML I/O

--- src/edu/ilstu/it/alarms/AlarmIO.java
+++ src/edu/ilstu/it/alarms/AlarmIO.java
@@ -1,15 +1,20 @@
 package edu.ilstu.it.alarms;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class AlarmIO {
 
-	private static final String PATH = "alarms.csv";
+	private static final String PATH = "alarms.xml";
 
 	public static void main(final String[] args) throws InterruptedException {
 		List<Alarm> alarms = new ArrayList<>();
@@ -27,34 +32,45 @@ public final class AlarmIO {
 	}
 
 	public static List<Alarm> loadAlarms() {
-		final List<Alarm> alarms = new ArrayList<>();
 		try {
-			final List<String> lines = Files.readAllLines(Paths.get(PATH));
-			for (final String line : lines) {
-				final String[] split = line.split(",");
-				final long time = Long.parseLong(split[0]);
-				alarms.add(split.length == 1 ? AlarmFactory.createAlarm(time) : AlarmFactory.createAlarm(time, line.substring(split[0].length() + 1)));
-			}
-		} catch (final IOException e) {
+			final JAXBContext jaxbContext = JAXBContext.newInstance(Alarms.class);
+			final Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			final Alarms alarms = (Alarms) jaxbUnmarshaller.unmarshal( new File(PATH) );
+			return alarms.getAlarms();
+		} catch (final JAXBException e) {
 			e.printStackTrace();
 		}
-		return alarms;
+		return null;
 	}
 
 	// TODO: remember to save to file after each new alarm is made
 	public static void saveAlarms(final List<Alarm> alarms) {
-		final List<String> lines = new ArrayList<>();
-		for (final Alarm alarm : alarms) {
-			final StringBuilder builder = new StringBuilder();
-			builder.append(alarm.getDate().getTime());
-			if (alarm.getMessage().isPresent())
-				builder.append(',').append(alarm.getMessage().get());
-			lines.add(builder.toString());
-		}
 		try {
-			Files.write(Paths.get(PATH), lines, StandardOpenOption.CREATE);
-		} catch (final IOException e) {
+			final File file = new File(PATH);
+			final JAXBContext context = JAXBContext.newInstance(Alarms.class);
+			final Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(new Alarms(alarms), file);
+		} catch (final JAXBException e) {
 			e.printStackTrace();
+		}
+	}
+
+	@XmlRootElement(name = "alarms")
+	@XmlAccessorType(XmlAccessType.FIELD)
+	private static class Alarms {
+		@XmlElement(name = "alarm")
+		private List<Alarm> alarms = null;
+
+		public Alarms() {
+		}
+
+		public Alarms(List<Alarm> alarms) {
+			this.alarms = alarms;
+		}
+
+		public List<Alarm> getAlarms() {
+			return alarms;
 		}
 	}
 }
